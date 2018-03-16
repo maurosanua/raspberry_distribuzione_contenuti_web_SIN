a1167f99b2b0a911f5e6f5b89668ff173a24498b
Marco <marco.benetti@sinergo.it>
2018-03-16 11:43:57 +0100

    cifratura seriale raspberry

--- classi/classe_AESCipher.php
+++ classi/classe_AESCipher.php
@@ -3,7 +3,8 @@ class classe_AESCipher {
     private $key;
     private $mode = "cbc";
     private $cipher = MCRYPT_RIJNDAEL_128;
-    private $BS = 16;
+    private $openssl_cipher = "AES-256-CBC";
+    private $BS = 16; //questo non puo' cambiare per ora
 
 
 
@@ -11,20 +12,30 @@ class classe_AESCipher {
         $this->key = $my_key;
     }
 
+    public function encrypt($msg){
+        return $this->openssl_encrypt($msg);
+    }
 
     public function decrypt($msg){
+        return $this->openssl_decrypt($msg);
+    }
+
+    public function mcrypt_decrypt($msg){
         $data = base64_decode($msg);
-        $iv= substr($data,0,16);
-        $mex= substr($data,16);
+        $iv= substr($data,0,$this->BS);
+        $mex= substr($data,$this->BS);
         $out = mcrypt_decrypt($this->cipher,$this->key,$mex,$this->mode,$iv);
         $temp = ord(substr($out,strlen($out)-1));
         $out = substr($out,0,strlen($out)-$temp);
         return $out;
     }
 
-    public function encrypt($msg){
-
-        $complement = $this->BS-strlen($msg)%$this->BS;
+    public function mcrypt_encrypt($msg){
+        if($this->BS>0){
+            $complement = $this->BS-strlen($msg)%$this->BS;
+        }else{
+            $complement = 0;
+        }
         for($i=0;$i<$complement;$i++){
             $msg.=chr($complement);
         }
@@ -32,7 +43,7 @@ class classe_AESCipher {
         $wasItSecure = false;
         $count= 0;
         while(!$wasItSecure){
-            $iv = openssl_random_pseudo_bytes(16, $wasItSecure);
+            $iv = openssl_random_pseudo_bytes($this->BS, $wasItSecure);
             $count++;
             if($count>100){
                 die();
@@ -44,6 +55,28 @@ class classe_AESCipher {
         return $out;
     }
 
-    
+    public function openssl_decrypt($msg) {
+        $data = base64_decode($msg);
+        $iv= substr($data,0,$this->BS);
+        $mex= substr($data,$this->BS);
+        $output = openssl_decrypt($mex, $this->openssl_cipher, $this->key, true, $iv);
+        return $output;
+    }
+
+    public function openssl_encrypt($msg){
+        $wasItSecure = false;
+        $count= 0;
+        while(!$wasItSecure){
+            $iv = openssl_random_pseudo_bytes($this->BS, $wasItSecure);
+            $count++;
+            if($count>100){
+                die();
+            }
+        }
+        $out  = openssl_encrypt($msg, $this->openssl_cipher, $this->key, true, $iv);
+        $out = base64_encode($iv.$out);
+        return $out;
+    }
+
 }
 ?>
