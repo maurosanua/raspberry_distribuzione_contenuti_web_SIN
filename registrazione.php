fbb3e516df4b6bcf72ca5d8325e36d1cba3ddaee
Francesco <francesco.sabato@sinergo.it>
2019-04-09 12:30:25 +0200

    test 3

--- registrazione.php
+++ registrazione.php
@@ -27,7 +27,7 @@ $k = SERIALE;
 			</div>
 		</div>
 			<div>
-				<h1>Registrazione nuovo dispositivo 2</h1>
+				<h1>Registrazione nuovo dispositivo 3</h1>
 			<br><p>Inquadra il codice QR o digita l'url sottostante per attivare questo dispositivo.<br>Se non visualizzi il QR code verifica la connessione a internet.</p>
 			</div>
 						
